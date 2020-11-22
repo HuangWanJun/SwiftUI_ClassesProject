1249eb276024250b963339971bfeb979c1eec680
Samuel Huang <samuel.huang@tonghaifinancial.com>
2020-11-22 16:45:32 +0800

    Initial Commit

---
+++ ClassesProject/ClassesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassesProjectApp.swift
+//  ClassesProject
+//
+//  Created by Samuel Huang on 22/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ClassesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
